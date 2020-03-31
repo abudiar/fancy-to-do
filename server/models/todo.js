dbb651bfc71bacec843a699053666e0cb1fc760c
Alan Budiarto <alanwbudiarto@gmail.com>
2020-03-31 12:03:09 +0700

    Updated migrations and models for User

--- server/models/todo.js
+++ server/models/todo.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     },
   }, {});
   Todo.associate = function (models) {
-    // associations can be defined here
+    Todo.belongsTo(models.User);
   };
   return Todo;
 };
---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        notEmpty: {
+          msg: "Username cannot be empty"
+        },
+        notNull: {
+          msg: "Username cannot be null"
+        }
+      }
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Password cannot be empty"
+        },
+        notNull: {
+          msg: "Password cannot be null"
+        }
+      }
+    }
+  }, {});
+  User.associate = function (models) {
+    User.hasMany(models.Todo);
+  };
+  return User;
+};
